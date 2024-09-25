c1aca2742b50b71978a7105284244e86ca5e0fe2
lnarote777 <lnarote777@iesrafaelalberti.es>
2024-09-25 11:30:58 +0200

    Funciones creadas. Hay que corregir errores en crearFicheroMedia

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,59 @@
 package org.example
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.File
+import java.nio.file.Files
+import java.nio.file.Path
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+fun main(){
+    val fileCotizaciones = Path.of("src/main/resources/cotizacion.csv")
+    val fileMedia = Path.of("src/main/resources/media.csv")
+    val br = Files.newBufferedReader(fileCotizaciones)
+    val bw = Files.newBufferedWriter(fileMedia)
+
+    val cotizaciones = leerCotizaciones(br)
+
+    crearFicheroMedia(cotizaciones, bw)
+
+}
+
+fun leerCotizaciones(br: BufferedReader): Map<String, List<String>>{
+    val diccionario = mutableMapOf<String, MutableList<String>>()
+
+    br.use { it.forEachLine {linea ->
+        val lineaSplit = linea.split(";")
+
+        for (datos in lineaSplit){
+            diccionario[datos] = mutableListOf()
+        }
+
+
+        for (i in lineaSplit.indices){
+            diccionario[lineaSplit[i]]?.add(lineaSplit[i])
+        }
     }
+
+
+    }
+
+    return diccionario
+}
+
+fun crearFicheroMedia(cotizaciones: Map<String, List<String>>,bw: BufferedWriter) {
+    val estadisticas = mutableMapOf<String, Triple<Double, Double, Double>>()
+
+    bw.use {
+        for ((nombre, datos) in cotizaciones){
+            val valores = datos.map { it.replace(",", ".").toDouble() }
+            val minimo: Double = valores.min()
+            val maximo: Double = valores.max()
+            val media: Double = valores.average()
+
+            estadisticas[nombre] = Triple(minimo, maximo, media)
+        }
+
+        estadisticas.forEach { linea -> it.write(linea.toString())  }
+    }
+
 }
